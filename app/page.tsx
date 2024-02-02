941f9f2c357000a0aff6fe049f23169b6743c13f
abhishek0wb <abhishek0wb@gmail.com>
2024-02-02 13:24:02 +0530

    fonts-edits

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,6 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
       </div>
       <AcmeLogo />
-        
-      
-      
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape}/>
@@ -33,7 +30,6 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-
           <Image
         src="/hero-desktop.png"
         width={1000}
